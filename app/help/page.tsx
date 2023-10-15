61521a68e9045a460db0988197ad4ad96b94d2e5
theunhackable <srirangasaipagala@gmail.com>
2023-10-15 16:59:06 +0530

    ignored typescript errors during build

--- app/help/page.tsx
+++ app/help/page.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 
 const Help = () => {
   return (
-    <main className='main'>
-    <h1 className="text-2xl font-extrabold">
-      Help 
-    </h1>
-  </main>
+    <div>
+      help
+    </div>
   )
 }
 
